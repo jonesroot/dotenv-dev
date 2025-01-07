07e8876842d1d96e5109dba762713ec0226b1e8b
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-01-07 17:31:39 +0700

    create dotenv_dev/__init__.py

---
+++ dotenv_dev/__init__.py
@@ -0,0 +1,11 @@
+from .__main__ import dotenv
+
+__name__ = "dotenv-dev"
+__version__ = "0.0.2"
+__author__ = "Lucifer"
+__author_contact__ = "https://t.me/LuciferReborns"
+__url__ = "https://github.com/jonesroot/dotenv.git"
+__description__ = """(Fork & Improved) Read value of .env(dotenv) into ENV values.
+
+
+Source Code: https://github.com/hikarine3/firstclass_dotenv.git"""
